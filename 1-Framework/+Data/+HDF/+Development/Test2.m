b173a2ffe6a586e77cced3ddd416bcd29fac7d88
Vincent Wilms <vwilms@outlook.com>
2019-06-19 10:47:54 +0200

    Add new HDF loading tests.

--- 1-Framework/+Data/+HDF/+Development/Test2.m
+++ 1-Framework/+Data/+HDF/+Development/Test2.m
@@ -2,8 +2,8 @@ clc
 clear
 close all
 
-dateTimeBegin           = datetime(2019, 04, 20, 'TimeZone', 'UTC');
-dateTimeEnd             = datetime(2019, 05, 02, 'TimeZone', 'UTC');
+dateTimeBegin           = datetime(2019, 01, 01, 'TimeZone', 'UTC');
+dateTimeEnd             = datetime(2019, 06, 01, 'TimeZone', 'UTC');
 hdfDataProvider         = Data.HDF.HdfDataProvider('M:\');
 
 setenv('HDF5_VDS_PREFIX', 'M:\')
@@ -17,19 +17,20 @@ datasetNameSet          = {'1 s_mean'
                            '1 s_mean'};
 
 dataInfoSet             = hdfDataProvider.LoadDatasets('/AIRPORT/AD8_PROTOTYPE/GENERAL_DAQ', variableNameSet, datasetNameSet, dateTimeBegin, dateTimeEnd);
+timeSet                 = (dateTimeBegin : 1/86400 : dateTimeEnd - 1/86400).';
 
 subplot(3, 1, 1)
-plot(dataInfoSet(1).Dataset);
+plot(timeSet, dataInfoSet(1).Dataset);
 title(strrep(dataInfoSet(1).NameSet(1), '_', '\_'));
 ylabel(dataInfoSet(1).Unit);
 
 subplot(3, 1, 2)
-plot(dataInfoSet(2).Dataset);
+plot(timeSet, dataInfoSet(2).Dataset);
 title(strrep(dataInfoSet(2).NameSet(1), '_', '\_'));
 ylabel(dataInfoSet(2).Unit);
 
 subplot(3, 1, 3)
-plot(dataInfoSet(3).Dataset / 1000 / 1000);
+plot(timeSet, dataInfoSet(3).Dataset / 1000 / 1000);
 title(strrep(dataInfoSet(3).NameSet(1), '_', '\_'));
 ylabel(dataInfoSet(3).Unit);
 
